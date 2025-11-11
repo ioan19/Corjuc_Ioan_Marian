b7d6ae9679f8a46d6197ba0770ba8a959a7b0d6d
Corjuc Ioan Marian <87379176+ioan19@users.noreply.github.com>
2025-11-11 15:51:35 +0200

    Add files via upload

---
+++ script_ex2.js
@@ -0,0 +1,14 @@
+let tranzactie1 = true;
+let tranzactie2 = false;
+
+console.log("Test IF (tranzactie1)");
+if (tranzactie1) {
+    console.log("Tranzactie acceptata!");
+}
+
+console.log("\n Test (tranzactie2)");
+if (tranzactie2) {
+    console.log("Tranzactie acceptata!"); 
+    } else {
+            console.log("Tranzactie refuzata!");
+        }
